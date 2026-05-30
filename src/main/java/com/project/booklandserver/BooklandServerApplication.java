b4a509abd46832d181f7831500a2e5f6c4f9387b
Tamara Stankovic <tasanadlanus@gmail.com>
2026-05-30 23:14:47 +0200

    Initial commit

---
+++ src/main/java/com/project/booklandserver/BooklandServerApplication.java
@@ -0,0 +1,13 @@
+package com.project.booklandserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooklandServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BooklandServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/booklandserver/BooklandServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.booklandserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooklandServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
